6995ddbd1d7b12d5774b457f2e41d5e06d9c4ffb
Rose <matthewcpharr@gmail.com>
2020-12-04 12:43:01 -0500

    foldering

---
+++ d05/day05.py
@@ -0,0 +1,18 @@
+# Changed my program structure today
+day = '05'
+inp = 'day' + day + 'input.txt'
+
+# Reading function goes here
+
+
+
+def solve(puzzle_input):
+    s1 = 0
+    s2 = 0
+    yield s1
+    yield s2
+
+if __name__ == "__main__":
+    with open(inp) as inputfile:
+        for s in solve(inp.read()):
+            print(s)
