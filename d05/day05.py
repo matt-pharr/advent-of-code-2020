c416f81478ecc31c316d704ff55fd7429b8f7940
Rose <matthewcpharr@gmail.com>
2020-12-05 00:52:17 -0500

    day05

--- d05/day05.py
+++ d05/day05.py
@@ -5,20 +5,59 @@
 # today i changed my program structure 
 
 import re
+
 day = '05'
 inp = 'day' + day + 'input.txt'
 
 # Reading function goes here
+def read_seats(puzzle_input:str):
+    return puzzle_input.splitlines()
+
+def findseat(s: str):
+    s = s.strip()
+    fb = s[:7]
+    lr = s[7:]
+    lp = list(range(128))
+    lp1 = list(range(8))
+    for c in fb:
+        if c == 'F':
+            lp = lp[:int(len(lp)/2)]
+        elif c == 'B':
+            lp = lp[int(len(lp)/2):]
+
+    for d in lr:
+        if d == 'L':
+            lp1 = lp1[:int(len(lp1)/2)]
+        elif d == 'R':
+            lp1 = lp1[int(len(lp1)/2):]
+    
+    return (lp[0],lp1[0])
 
 
 
 def solve(puzzle_input):
-    s1 = 0
-    s2 = 0
+    a = [findseat(x) for x in read_seats(puzzle_input)]
+    b = [(y[0]*8 + y[1]) for y in a]
+
+    fullset = set()
+    for i in range(128):
+        for j in range(8):
+            fullset.add((i,j))
+
+
+    s1 = max(b)
+    s2 = fullset - set(a)
+
+    for s in s2:
+        if re.match('\d{2}$', str(s[0])) is not None:
+            print(s)
+
     yield s1
     yield s2
 
+    #literally printed this set difference and jus looked for a seat in the middle lol -- the 69 ended up sticking out
+
 if __name__ == "__main__":
     with open(inp) as inputfile:
-        for s in solve(inp.read()):
+        for s in solve(inputfile.read()):
             print(s)
