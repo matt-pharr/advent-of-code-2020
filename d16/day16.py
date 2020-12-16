1cf0a61b9f4c8f369be6964c28c6c3f9a5495a62
Rose <matthewcpharr@gmail.com>
2020-12-15 23:59:06 -0500

    day16prep

---
+++ d16/day16.py
@@ -0,0 +1,23 @@
+# Matthew Pharr
+# Advent of Code 2020
+# Rensselaer Polytechnic Institute
+import os 
+import re
+
+day = '00'
+inp = os.path.dirname(os.path.realpath(__file__)) + '/day' + day + 'input.txt'
+
+# Reading function goes here
+
+
+
+def solve(puzzle_input):
+    s1 = 0
+    s2 = 0
+    yield s1
+    yield s2
+
+if __name__ == "__main__":
+    with open(inp) as iofile:
+        for s in solve(iofile.read()):
+            print(s)
